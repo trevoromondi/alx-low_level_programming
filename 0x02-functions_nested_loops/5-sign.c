6f25a50a68df1b9fc947aa5e4896822d0de6062c
trevoromondi <otienomondi23@gmail.com>
2022-03-15 11:26:01 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign - print sign of number
+ * Return: sign
+ */
+int print_sign(int n)
+{
+	int sign;
+
+	if (n > 0)
+	{
+		sign = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		sign = 0;
+		_putchar('0');
+
+	}
+	else
+	{
+		sign = -1;
+		_putchar('-');
+	}
+	return (sign);
+}
