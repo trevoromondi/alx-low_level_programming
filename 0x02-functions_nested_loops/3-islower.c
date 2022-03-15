11b945e1581951f500973dc026938e3e66e410d4
trevoromondi <otienomondi23@gmail.com>
2022-03-15 10:13:40 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islower - checks if letters are lowercase
+ * @x : character to test case
+ * Return: 0 or 1
+ */
+int _islower(int x)
+{
+	return (x >= 97 && x <= 122);
+}
