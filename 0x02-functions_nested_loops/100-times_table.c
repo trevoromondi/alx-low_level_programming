026f1858fec6fe66b7e2bdccf2e17fb6d3e59a2b
trevoromondi <otienomondi23@gmail.com>
2022-03-15 13:55:21 -0700

    12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -23,11 +23,13 @@ void print_times_table(int n)
 			{
 				_putchar(',');
 				_putchar(' ');
+				_putchar(' ');
 				_putchar('0' + rep);
 			}
 			else if (rep < 100)
 			{
 				_putchar(',');
+				_putchar(' ');
 				_putchar('0' + rep / 10);
 				_putchar('0' + rep % 10);
 			}
