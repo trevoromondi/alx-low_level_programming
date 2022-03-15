290f4a19c62e3c2996b42d02755221e040e220dc
trevoromondi <otienomondi23@gmail.com>
2022-03-15 04:21:05 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - prints _putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c[9] = "_putchar";
+	int i = 0;
+	while (i < 8)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
