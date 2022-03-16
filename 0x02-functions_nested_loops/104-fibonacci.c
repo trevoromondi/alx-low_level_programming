dd16505d161ce2982415e42efecef0edf5466cb3
trevoromondi <otienomondi23@gmail.com>
2022-03-16 12:32:26 -0700

    16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints the first 98 fibonacci numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+	unsigned long int a = 0, b = 1, next = 0;
+
+	while (i < 98)
+	{
+		next = a + b;
+		a = b;
+		b = next;
+		printf("%lu", next);
+
+		if (i < 97)
+			printf(", ");
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
