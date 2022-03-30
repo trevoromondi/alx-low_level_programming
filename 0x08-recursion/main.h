d219cb94e5d72de71b6f235cc07dbc6a748e9f54
trevoromondi <otienomondi23@gmail.com>
2022-03-30 12:42:52 -0700

    Your subconscious is looking for the dreamer

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,4 +4,5 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
+int square (int n, int vl);
 int _sqrt_recursion(int n);
