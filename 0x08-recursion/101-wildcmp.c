e8e673eee4b350d21a0a3364b76d84a7a9307cbd
trevoromondi <otienomondi23@gmail.com>
2022-03-30 20:21:10 -0700

    8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -38,14 +38,14 @@ int checker(char *s1, char *s2, int a, int b, int wildUsed)
 				return (checker(s1, s2, a, b + 1, b));
 			else if (s2[b + 1] == s1[a])
 				return (checker(s1, s2, a, b + 1, b));
-			else if(s1[a + 1] != s2[b + 1])
+			else if (s1[a + 1] != s2[b + 1])
 				return (checker(s1, s2, a + 1, b, b));
 			else if (s1[a + 1] == s2[b + 1])
 				return (checker(s1, s2, a + 1, b + 1, b));
 		}
 		else if ((s1[a] == s2[b]) || (s2[b] == '*' && s2[b + 1] == s1[a + 1]))
 			return (checker(s1, s2, a + 1, b + 1, wildUsed));
-		
+
 		if (wildUsed == -1)
 			return (0);
 		
