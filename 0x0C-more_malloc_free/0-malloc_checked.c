901dd37dd3fbe887798d2b843e0ad1e681a60946
trevoromondi <otienomondi23@gmail.com>
2022-04-07 12:49:53 -0700

    Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+/**
+ * *malloc_checked - allocates memory using malloc
+ * @b: int
+ * Return: pointer to the array
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *x = malloc(b);
+
+	if (m == 0)
+		exit(98);
+
+	return (m);
+}
