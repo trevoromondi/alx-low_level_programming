3231edde4beca002bd41467644658599e512729b
trevoromondi <otienomondi23@gmail.com>
2022-04-07 13:26:46 -0700

    _calloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,5 +5,7 @@
 void *malloc_checked(unsigned int b);
 unsigned int _strlen(char *s);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
 
 #endif
