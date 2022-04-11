45b828e260f3a22c86ace0ce762439f9ace36e09
trevoromondi <otienomondi23@gmail.com>
2022-04-11 13:23:41 -0700

    A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,6 +16,8 @@ typedef struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
 dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
