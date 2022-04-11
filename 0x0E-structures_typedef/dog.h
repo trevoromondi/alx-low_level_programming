5893360bc80fdbe4970918fe9b2c328026d2a0c2
trevoromondi <otienomondi23@gmail.com>
2022-04-11 11:19:25 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - definition of a dog
+ * @name: char string
+ * @age: int
+ * @owner: char string
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+#endif
