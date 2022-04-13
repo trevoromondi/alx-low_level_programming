e9ef68508a4fad43447d26e308c047d81213c9b8
trevoromondi <otienomondi23@gmail.com>
2022-04-13 13:32:25 -0700

    3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - addition operator
+ * @a: int
+ * @b: int
+ * Return: sum of 2 nums
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - sub operator
+ * @a: int
+ * @b: int
+ * Return: diff of 2 num
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - product op
+ * @a: int
+ * @b: int
+ * Return: prod of nums
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - div op
+ * @a: int
+ * @b: int
+ * Return: div of nums
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulus of 2 nums
+ * @a: int
+ * @b: int
+ * Return: mod
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
