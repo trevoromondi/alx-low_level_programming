e3ff20790050794b69c9891d52fcefd7be3685d6
trevoromondi <otienomondi23@gmail.com>
2022-04-13 12:40:35 -0700

    2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,7 +11,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int x;
 
-	if (size < 1 || array == NULL || cmp = NULL)
+	if (size < 1 || array == NULL || cmp == NULL)
 		return (-1);
 
 	for (x = 0; x < size; x++)
