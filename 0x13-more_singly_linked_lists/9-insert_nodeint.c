d9fa6f4f68ef4df620e694de04a866befd898285
trevoromondi <otienomondi23@gmail.com>
2022-04-26 14:26:41 -0700

    9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -5,7 +5,7 @@
  * @head: head of linked list
  * @idx: index to insert the node
  * @n: new node data
- * Return new node
+ * Return: new node
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
