23a669f94a9cf9b2c2780d4c9715a205e460dd8a
trevoromondi <otienomondi23@gmail.com>
2022-09-20 19:03:32 -0700

    8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: array pointer
+ * @n: integer
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int x;
+
+	x = 0;
+
+	for (n--; n >= 0; n--, x++)
+	{
+		printf("%d", a[x]);
+		if (n > 0)
+		{
+			printf(", ");
+		}
+	}
+
+	printf("\n");
+}
