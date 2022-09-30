12c81cc440342e20f6f512a009354d698f251dfd
trevoromondi <otienomondi23@gmail.com>
2022-09-30 16:52:24 -0700

    0

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower - checks if letters are lowercase
+ * @x: character to test case
+ * Return: 0 or 1
+ */
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
